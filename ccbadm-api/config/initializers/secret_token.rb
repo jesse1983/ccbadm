cd0ef17003447a1924438c9f05cd734bc46d8538
Jesse Pinheiro <jessebpinheiro@hotmail.com>
2016-09-07 10:01:30 -0300

    Secret key

---
+++ ccbadm-api/config/initializers/secret_token.rb
@@ -0,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ccbonline::Application.config.secret_key_base = '329c8c15894bec1a9816ce4bcd5a0b5f198f8bcfb2451fdcc928d69654631fb3c54170e8f68f2a5574b1881309fef729e1458ac09738e4659e29fed85f5fbc50'
